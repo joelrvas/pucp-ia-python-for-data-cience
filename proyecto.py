45ee0efb4bdb65ff8e4f8a8f7b0b067237c15968
Joel Ramirez <joel.rz.sw@gmail.com>
2024-10-27 14:10:01 -0500

    JRV: initial commit , read data sets

---
+++ proyecto.py
@@ -0,0 +1,10 @@
+# DataSet: https://www.datosabiertos.gob.pe/dataset/centros-de-vacunacion
+#Campo	Descripción	Tipo de dato	Tamaño
+#id_centro_vacunacion	identificador	Numérico	4
+#id_ubigeo	Puntero a tabla de tb_ubigeos	Numérico 	4
+#nombre	Nombre del Centro de Vacunación	Texto	
+#entidad_administra	Entidad que administra el Centro de Vacunación	Texto	
+#latitud	Latitud del Distrito	Numérico	14,8
+#longitud	Longitud del Distrito	Numérico	14,8
+
+import pandas as pd
